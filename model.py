8e1476221e6e6a7376dbe5694e1cf6b8981d321d
Torbet <guy@torbet.co>
2025-02-03 19:40:56 +0000

    updates

--- model.py
+++ model.py
@@ -14,11 +14,16 @@ class Transformer(nn.Module):
     nhead: int = 8,
     num_layers: int = 6,
     dim_feedforward: int = 2048,
-    dropout: float = 0.1
+    dropout: float = 0.1,
+    evals: bool = True,
+    times: bool = True,
   ):
     super().__init__()
 
-    d_model = hidden_dim * 3
+    self.evals = evals
+    self.times = times
+
+    d_model = hidden_dim * (1 + evals + times)
 
     self.move_encoder = nn.Sequential(
       nn.Conv3d(in_channels, 32, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
@@ -35,32 +40,32 @@ class Transformer(nn.Module):
     )
 
     # TODO: Experiment with MaxPool1d
-
-    self.eval_encoder = nn.Sequential(
-      nn.Conv1d(1, 32, kernel_size=3, padding=1),
-      nn.BatchNorm1d(32),
-      nn.ReLU(),
-      nn.Conv1d(32, 64, kernel_size=3, padding=1),
-      nn.BatchNorm1d(64),
-      nn.ReLU(),
-      nn.Conv1d(64, hidden_dim, kernel_size=3, padding=1),
-      nn.BatchNorm1d(hidden_dim),
-      nn.ReLU(),
-    )
+    if self.evals:
+      self.eval_encoder = nn.Sequential(
+        nn.Conv1d(1, 32, kernel_size=3, padding=1),
+        nn.BatchNorm1d(32),
+        nn.ReLU(),
+        nn.Conv1d(32, 64, kernel_size=3, padding=1),
+        nn.BatchNorm1d(64),
+        nn.ReLU(),
+        nn.Conv1d(64, hidden_dim, kernel_size=3, padding=1),
+        nn.BatchNorm1d(hidden_dim),
+        nn.ReLU(),
+      )
 
     # TODO: Experiment with MaxPool1d
-
-    self.time_encoder = nn.Sequential(
-      nn.Conv1d(1, 32, kernel_size=3, padding=1),
-      nn.BatchNorm1d(32),
-      nn.ReLU(),
-      nn.Conv1d(32, 64, kernel_size=3, padding=1),
-      nn.BatchNorm1d(64),
-      nn.ReLU(),
-      nn.Conv1d(64, hidden_dim, kernel_size=3, padding=1),
-      nn.BatchNorm1d(hidden_dim),
-      nn.ReLU(),
-    )
+    if self.times:
+      self.time_encoder = nn.Sequential(
+        nn.Conv1d(1, 32, kernel_size=3, padding=1),
+        nn.BatchNorm1d(32),
+        nn.ReLU(),
+        nn.Conv1d(32, 64, kernel_size=3, padding=1),
+        nn.BatchNorm1d(64),
+        nn.ReLU(),
+        nn.Conv1d(64, hidden_dim, kernel_size=3, padding=1),
+        nn.BatchNorm1d(hidden_dim),
+        nn.ReLU(),
+      )
 
     self.positional_encoding = PositionalEncoding(d_model, dropout, max_len=40)
 
@@ -76,10 +81,15 @@ class Transformer(nn.Module):
     moves = self.move_encoder(moves)
     moves = moves.permute(0, 2, 1, 3, 4).reshape(BS, T, -1)
 
-    evals = self.eval_encoder(evals.unsqueeze(1)).permute(0, 2, 1)
-    times = self.time_encoder(times.unsqueeze(1)).permute(0, 2, 1)
+    combined = moves
 
-    combined = torch.cat([moves, evals, times], dim=-1)
+    if self.evals:
+      evals = self.eval_encoder(evals.unsqueeze(1)).permute(0, 2, 1)
+      combined = torch.cat([combined, evals], dim=-1)
+    if self.times:
+      times = self.time_encoder(times.unsqueeze(1)).permute(0, 2, 1)
+      combined = torch.cat([combined, times], dim=-1)
+
     combined = self.positional_encoding(combined)
 
     output = self.transformer_encoder(combined)
@@ -107,28 +117,30 @@ class PositionalEncoding(nn.Module):
 
 
 class ConvLSTM(nn.Module):
-  def __init__(self):
+  def __init__(self, in_channels: int = 12, evals: bool = True, times: bool = True):
     super().__init__()
-    self.conv1 = nn.Conv2d(12, 64, 3, padding=1)
-    self.bn1 = nn.BatchNorm2d(64)
-    self.pool1 = nn.MaxPool2d(2)
-    self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
-    self.bn2 = nn.BatchNorm2d(128)
-    self.pool2 = nn.MaxPool2d(2)
-    self.conv3 = nn.Conv2d(128, 256, 3, padding=1)
-    self.bn3 = nn.BatchNorm2d(256)
-    self.pool3 = nn.MaxPool2d(2)
-    self.lstm = nn.LSTM(257, 512, 2, batch_first=True, bidirectional=True, dropout=0.5)
-    self.fc = nn.Sequential(nn.Linear(1024, 512), nn.ReLU(), nn.Dropout(0.5), nn.Linear(512, 4))
+    self.evals = evals
+    self.times = times
 
-  def forward(self, moves: torch.Tensor, evals: torch.Tensor) -> torch.Tensor:
+    self.conv1 = nn.Conv2d(in_channels, 64, 3)
+    self.conv2 = nn.Conv2d(64, 128, 3)
+    self.conv3 = nn.Conv2d(128, 256, 3)
+    dim = 1024 + evals + times
+    self.lstm = nn.LSTM(dim, 256, batch_first=True, bidirectional=True)
+    # self.fc = nn.Sequential(nn.Linear(1024, 512), nn.ReLU(), nn.Dropout(0.5), nn.Linear(512, 4))
+    self.fc = nn.Linear(512, 4)
+
+  def forward(self, moves: torch.Tensor, evals: torch.Tensor, times: torch.Tensor) -> torch.Tensor:
     BS, T, C, H, W = moves.shape
     x = moves.view(BS * T, C, H, W)
-    x = self.pool1(F.relu(self.bn1(self.conv1(x))))
-    x = self.pool2(F.relu(self.bn2(self.conv2(x))))
-    x = self.pool3(F.relu(self.bn3(self.conv3(x))))
-    x = F.adaptive_avg_pool2d(x, 1).view(BS, T, -1)
-    x = torch.cat([x, evals.unsqueeze(-1)], dim=-1)
+    x = F.relu(self.conv1(x))
+    x = F.relu(self.conv2(x))
+    x = F.relu(self.conv3(x))
+    x = x.view(BS, T, -1)
+    if self.evals:
+      x = torch.cat([x, evals.unsqueeze(-1)], dim=-1)
+    if self.times:
+      x = torch.cat([x, times.unsqueeze(-1)], dim=-1)
     x, _ = self.lstm(x)
     return self.fc(x[:, -1])
 
@@ -145,7 +157,6 @@ class Dense(nn.Module):
     self.layers = nn.Sequential(*layers)
 
   def forward(self, x: torch.Tensor) -> torch.Tensor:
-    x = x.permute(0, 2, 1, 3, 4)
     x = x.reshape(x.size(0), -1)
     return self.layers(x)
 
